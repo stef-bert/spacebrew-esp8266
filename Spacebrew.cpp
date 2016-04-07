cdf740097b7ef425eca0db481c01336f938c321d
stef-bert <github@steffenhartwig.de>
2016-04-07 15:16:43 +0200

    removing doubled line
    what a massive fail.

--- Spacebrew.cpp
+++ Spacebrew.cpp
@@ -135,7 +135,6 @@ void Spacebrew::sendConfig(){
 }
 
 void Spacebrew::webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-    switch(type) {
 	switch(type) {
 		case WStype_CONNECTED:
 			onWSOpen();
@@ -150,7 +149,6 @@ void Spacebrew::webSocketEvent(WStype_t type, uint8_t * payload, size_t length) 
 			onWSError((char*) payload);
 			break;
 	}
-    }
 }
 
 void Spacebrew::onWSOpen(){
